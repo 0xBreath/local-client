8680aca82aaffde03aa8434536fa8d822ab7e4fe
bowtiedbreath <bowtiedbreath@protonmail.com>
2022-05-18 11:37:25 -0400

    remove CORS

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -116,7 +116,7 @@ const HomeNew = (props: any) => {
         const TWITTER_ROUTE = USER_ROUTE + `/${pubkey}/twitter/authorize`;
         let res = await fetch(TWITTER_ROUTE, {
           method : "Get",
-          mode: 'cors',
+          mode: 'no-cors',
           redirect: 'follow'
         });
         console.log('? ', res.redirected);
